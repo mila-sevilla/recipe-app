582eee5a634316bbbe3177cab4d342f3a72db3f6
msevilla <m.sevillazhou@gmail.com>
2019-05-07 20:53:28 +0200

    Updates

---
+++ precache-manifest.a302cb85792ac60ae13b1274721a33ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cada2d275681fe90d0c9acf0ba3dcc2",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "2831892d7676ae90693e",
+    "url": "/recipe-app/static/css/main.b5a13d22.chunk.css"
+  },
+  {
+    "revision": "87b23792489ac3eae909",
+    "url": "/recipe-app/static/js/2.84bb2fd3.chunk.js"
+  },
+  {
+    "revision": "2831892d7676ae90693e",
+    "url": "/recipe-app/static/js/main.572848fe.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  },
+  {
+    "revision": "28917e2e5bb11ca8fd8b8866eafdb212",
+    "url": "/recipe-app/static/media/arrow_down.28917e2e.svg"
+  }
+]);
