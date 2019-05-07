0dfb75b32f74b97f5d39a9498fb91ad1058c190d
msevilla <m.sevillazhou@gmail.com>
2019-05-07 20:19:45 +0200

    Updated fonts

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import style from './recipe.module.css'
 import Toggle from './Toggle'
+import { CSSTransition } from 'react-transition-group';
+
 
 const Recipe = ({title,image, ingredients}) => {
 
@@ -8,16 +10,18 @@ const Recipe = ({title,image, ingredients}) => {
         <div className={style.recipe}>
             <div className={style.recipe_cover}> 
                 <img className={style.image}src={image} alt=''/>
+                <h2> {title} </h2>
             </div>
-            <h2> {title} </h2>
             <Toggle 
                 render={({on, toggle}) => (
                     <div className={style.expand_wrapper}>
                         <button className={style.expand_button} onClick={toggle} aria-label='expand'> Show Recipe </button>
                         {on && 
                             <ul>
-                                {ingredients.map(ingredient => (
-                                    <li>{ingredient.text} </li>
+                                {ingredients.map((ingredient, index) => (
+                                    <CSSTransition in={on} timeout={200} classNames="appear"> 
+                                        <li key={index}>{ingredient.text} </li>
+                                    </CSSTransition>
                                 ))}
                             </ul>
                         }
