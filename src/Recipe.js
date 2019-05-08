5fee3522849937a3df4fbb76021d5403bbcca6d7
msevilla <m.sevillazhou@gmail.com>
2019-05-08 12:53:19 +0200

    Rewrote render props to hooks

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './recipe.module.css'
-import Toggle from './Toggle'
 import { CSSTransition } from 'react-transition-group';
 
-
 const Recipe = ({title,image, ingredients}) => {
+    
+    const [showRecipe, setShowRecipe] = useState(false)
+    const toggleRecipe = () => setShowRecipe(!showRecipe)
 
     return(
         <div className={style.recipe}>
@@ -12,21 +13,28 @@ const Recipe = ({title,image, ingredients}) => {
                 <img className={style.image}src={image} alt=''/>
                 <h2> {title} </h2>
             </div>
-            <Toggle 
-                render={({on, toggle}) => (
-                    <div className={style.expand_wrapper}>
-                        <button className={style.expand_button} onClick={toggle} aria-label='expand'> Show Recipe </button>
-                        {on && 
-                            <ul>
-                                {ingredients.map((ingredient, index) => (
-                                    <CSSTransition in={on} timeout={200} classNames="appear"> 
-                                        <li key={index}>{ingredient.text} </li>
-                                    </CSSTransition>
-                                ))}
-                            </ul>
-                        }
-                    </div>
-            )} /> 
+
+            <div className={style.expand_wrapper}>
+                <button 
+                    className={style.expand_button} 
+                    type="button" 
+                    aria-label='expand'
+                    onClick={toggleRecipe}>{showRecipe ? 'Hide Recipe' : 'Show Recipe'}
+                </button>
+                
+                <CSSTransition 
+                    in={showRecipe} 
+                    timeout={200} 
+                    classNames="expand"
+                    unmountOnExit
+                > 
+                    <ul key={ingredients}> {ingredients.map((ingredient, index) => (
+                        <li key={index}>
+                            {ingredient.text} 
+                        </li>))}
+                    </ul>
+                </CSSTransition>
+            </div>
         </div>
     )
 }
